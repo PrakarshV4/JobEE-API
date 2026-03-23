b4effcd7c14fddf40b5ff8ca45c7b08d94962593
Prakarsh <prakarsh1248@gmail.com>
2026-03-23 17:57:28 +0530

    created basic setup for our Jobee-API

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv')
+
+// Setting up config.env file variables
+dotenv.config({
+    path: './config/config.env'
+})
+
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT} in ${process.env.NODE_ENV}`)
+})
